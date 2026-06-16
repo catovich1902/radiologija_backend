dc79d2fdc7affd56979b1a04180408b7287344e4
Hana <hanicacatovic@gmail.com>
2026-06-16 10:18:45 +0200

    Add superuser creation

--- build.sh
+++ build.sh
@@ -3,3 +3,11 @@ set -o errexit
 pip install -r requirements.txt
 python manage.py collectstatic --no-input
 python manage.py migrate
+python manage.py shell -c "
+from django.contrib.auth.models import User
+if not User.objects.filter(username='admin').exists():
+    User.objects.create_superuser('admin', 'hanicacatovic@gmail.com', 'admin1234')
+    print('Superuser kreiran')
+else:
+    print('Superuser vec postoji')
+"
