e71037e1e2dc20e61292af5a18d5288b06b4c23a
Hana <hanicacatovic@gmail.com>
2026-06-16 10:33:59 +0200

    Fix CORS

--- radiologija_backend/settings.py
+++ radiologija_backend/settings.py
@@ -141,7 +141,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CORS_ALLOWED_ORIGINS = [
     'http://localhost:3000',
-    'https://tvoj-frontend.vercel.app',
+    'https://radiologija-frontend.vercel.app',
 ]
 
 CORS_ALLOW_CREDENTIALS = True
